4b5ee6cba10a754b43c1175868597094dded6f8a
dohb128 <202141093@itc.ac.kr>
2023-12-04 00:56:11 +0900

    week13 v4.2

---
+++ week13/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a []string
+	var b []bool
+
+	//a = make([]string, 4, 5)
+
+	b = append(b, true)
+
+	fmt.Printf("%#V %#V\n", a, b)
+	fmt.Println(a, len(a), cap(a))
+	fmt.Println(b, len(b), cap(b))
+}
